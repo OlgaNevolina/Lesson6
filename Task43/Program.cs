0af6e366a13e26284c6e5a085b24df08e7cd3e79
nevol78@mail.ru <nevol78@mail.ru>
2022-05-30 00:54:20 +0300

    Task43

---
+++ Task43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+
+double  x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
