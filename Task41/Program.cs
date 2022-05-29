cab2390d6be22c59445051ffc7114b8cda227572
nevol78@mail.ru <nevol78@mail.ru>
2022-05-30 00:50:56 +0300

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целые числа через пробел: ");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+    {
+      count++;  
+    }
+}
+Console.WriteLine($"Чисел больше нуля: {count}");
